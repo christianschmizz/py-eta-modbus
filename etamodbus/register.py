4baf055c4639b034d6e15c768c32f1c07547acee
Christian Schmitz <88237+christianschmizz@users.noreply.github.com>
2023-12-05 08:15:58 +0100

    Separate state handling

--- etamodbus/register.py
+++ etamodbus/register.py
@@ -9,9 +9,9 @@ class Register(collections.namedtuple('Register', 'id high low')):
 
 class RegisterDescription(collections.namedtuple('RegisterDescription', 'addr name node_id fub_id fkt_id io_id var_id scale unit min max default')):
     def value(self, reg_vals):
-        if "12153" == self.var_id:
+        if '12153' == self.var_id:
             return SimpleNumericValue.from_register(reg_vals, unit="h", scale=3600)
-        elif not self.unit and ('Zustand' in self.name or 'Anforderung' in self.name):
+        elif self.is_state:
             return TextValue.from_register(reg_vals)
         else:
             return SimpleNumericValue.from_register(reg_vals, unit=self.unit, scale=self.scale)
@@ -35,3 +35,7 @@ class RegisterDescription(collections.namedtuple('RegisterDescription', 'addr na
     @property
     def uri(self):
         return '/'.join([self.node_id, self.fub_id, self.fkt_id, self.io_id, self.var_id])
+
+    @property
+    def is_state(self):
+        return not self.unit and (('Zustand' in self.name or 'Anforderung' in self.name) or self.var_id == '12000')
